9635634fba65bef65ec641d9f12f1364f677f809
niantongdong <niantongdong@gmail.com>
2021-10-11 23:38:09 -0400

    dummy data

--- API/api.py
+++ API/api.py
@@ -1,15 +1,31 @@
 import flask
-
+import json
+from flask import request, jsonify
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
+
 
 
 @app.route('/', methods=['GET'])
 def hello():
     return "Hello World!"
 
-@app.route('/<string:name>', methods=['GET'])
-def helloName(name):
-    return "Hello " + name + "!" 
+# Get permit result
+# if id is provided, return certain result.
+# Otherwise, return all
+#  /permit?id= <id>
+@app.route('/permit', methods=['GET'])
+def getPermit():
+    with open('dummy.json','r') as jsonfile:
+        permitData = json.loads(jsonfile.read())
+    if 'id' in request.args:
+        id = int(request.args['id'])
+    else:
+        return json.dumps(permitData)
+    
+    for permit in permitData:
+        if permit['id'] == id:
+            return jsonify(permit)
 
-app.run()
+if __name__ == '__main__':
+    app.run(debug=True)
